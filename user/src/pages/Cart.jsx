1240e308ca8c58a1d7a7a734e1298c779be0fd5d
Subhash <120456241+subhashbairwa@users.noreply.github.com>
2025-08-06 15:19:44 +0530

    Update Cart.jsx

--- user/src/pages/Cart.jsx
+++ user/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
-import HubSpot from "next-auth/providers/hubspot";
+// import HubSpot from "next-auth/providers/hubspot";
 
 const Cart = () => {
   const { products, currency, cartItems, updateQuantity, navigate } =
